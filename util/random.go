dff51b036c13dc2663c98ce84b9c243d10c834a7
ngovantuan <ngovantuan02092003@gmail.com>
2024-12-05 13:49:35 +0700

    feat: add API transfer money with a custom params validator

--- util/random.go
+++ util/random.go
@@ -36,7 +36,7 @@ func RandomMoney() int64 {
 }
 
 func RandomCurrency() string {
-	currencies := []string{"EUR", "USD", "CAD"}
+	currencies := []string{USD, EUR, CAD}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
 }
