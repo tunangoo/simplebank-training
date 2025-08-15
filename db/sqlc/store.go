5321e3844ab2ad759795cd1065b9215e73a7d53f
tunangoo <ngovantuan02092003@gmail.com>
2025-08-15 23:15:31 +0700

    fix: correct JSON tag syntax in TransferTxResult struct

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -57,7 +57,7 @@ type TransferTxParams struct {
 }
 
 type TransferTxResult struct {
-	Transfer    Transfer `json:"transfer`
+	Transfer    Transfer `json:"transfer"`
 	FromAccount Account  `json:"from_account"`
 	ToAccount   Account  `json:"to_account"`
 	FromEntry   Entry    `json:"from_entry"`
